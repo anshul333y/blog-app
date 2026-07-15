79c7a3a03757b89c65d47197d2407f91960966f9
Anshul Yadav <anshul333y@gmail.com>
2026-07-16 03:45:25 +0530

    feat: add signin and signup routes

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,13 +1,61 @@
 import { Hono } from "hono";
+import getPrisma from "./prismaFunction";
+import { sign } from "hono/jwt";
 
-const auth = new Hono();
+const auth = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-auth.post("/signup", (c) => {
-  return c.text("Hello Hono!");
+auth.post("/signup", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const secret = c.env.JWT_SECRET;
+    const token = await sign({ id: user.id }, secret);
+
+    return c.json({
+      jwt: token,
+    });
+  } catch (error) {
+    return c.text("something went wrong");
+  }
 });
 
-auth.post("/signin", (c) => {
-  return c.text("Hello Hono!");
+auth.post("/signin", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (user) {
+      const secret = c.env.JWT_SECRET;
+      const token = await sign({ id: user.id }, secret);
+
+      return c.json({
+        jwt: token,
+      });
+    }
+
+    return c.text("something went wrong");
+  } catch (error) {
+    return c.text("something went wrong");
+  }
 });
 
 export default auth;
--- backend/src/prismaFunction.ts
+++ backend/src/prismaFunction.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const getPrisma = (database_url: string) => {
+const getPrisma = (database_url: string) => {
   const prisma = new PrismaClient({
-    datasourceUrl: database_url,
+    accelerateUrl: database_url,
   }).$extends(withAccelerate());
   return prisma;
 };
+
+export default getPrisma;
