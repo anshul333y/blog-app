2a80b3d9afcf91d5351bc2b6725a07d9344c0432
Anshul Yadav <anshul333y@gmail.com>
2026-07-17 02:33:48 +0530

    feat: add auth middleware

--- backend/src/blog.ts
+++ backend/src/blog.ts
@@ -1,6 +1,24 @@
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
-const blog = new Hono();
+const blog = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+blog.use("/*", async (c, next) => {
+  const header = c.req.header("Authorization");
+  const token = header.split(" ")[1];
+
+  const response = await verify(token, c.env.JWT_SECRET, "RS256");
+
+  if (response.id) {
+    await next();
+  }
+  return c.text("something went wrong");
+});
 
 blog.post("/", (c) => {
   return c.text("Hello Hono!");
