966657a020cbf101693c607786b257dde58c90c3
Anshul Yadav <anshul333y@gmail.com>
2026-07-18 01:32:11 +0530

    feat: add react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <>
-      <div>hi there from react with vite</div>
-      <h1 className="text-3xl text-green-300 font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
     </>
   );
 }
