f4155f57d44b6d4843b8b42b5c6ddd325ca70870
Anshul Yadav <anshul333y@gmail.com>
2026-07-18 00:41:17 +0530

    feat: add tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <div>hi there from react with vite</div>
+      <h1 className="text-3xl text-green-300 font-bold underline">
+        Hello world!
+      </h1>
     </>
   );
 }
