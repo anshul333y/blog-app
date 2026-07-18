42c5cb65911519367100c60e8da17410b16c9700
Anshul Yadav <anshul333y@gmail.com>
2026-07-18 18:27:56 +0530

    feat: add cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import auth from "./routes/auth";
 import blog from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api/v1");
+
+app.use("/*", cors());
 
 app.route("/auth", auth);
 
